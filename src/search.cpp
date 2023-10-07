76f4cd1fac9acf520b3a1d3e95adbc9c3072107d
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-07 09:40:54 +0800

    Bench 9638702

--- src/search.cpp
+++ src/search.cpp
@@ -432,12 +432,6 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, S_ThreadData* td
 	if (ttHit)
 		ttPv = pvNode || (tte.wasPv_flags >> 2);
 
-	// IIR by Ed Schroder (That i find out about in Berserk source code)
-	// http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
-	// https://github.com/jhonnold/berserk/blob/dd1678c278412898561d40a31a7bd08d49565636/src/search.c#L379
-	if (depth >= 4 && ttFlag == HFNONE && !excludedMove)
-		depth--;
-
 	// If we are in check or searching a singular extension we avoid pruning before the move loop
 	if (in_check || excludedMove) {
 		ss->static_eval = eval = score_none;
@@ -539,6 +533,13 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, S_ThreadData* td
 	}
 
 moves_loop:
+
+	// IIR by Ed Schroder (That i find out about in Berserk source code)
+	// http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
+	// https://github.com/jhonnold/berserk/blob/dd1678c278412898561d40a31a7bd08d49565636/src/search.c#L379
+	if (depth >= 4 && ttFlag == HFNONE && !excludedMove)
+		depth--;
+
 	// create move list instance
 	S_MOVELIST move_list[1];
 
