e2177418b0fe131acaf4ced5b1d6765f87eb68b9
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-09 10:11:22 +0800

    Bench 8657145

--- src/search.cpp
+++ src/search.cpp
@@ -565,7 +565,8 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, S_ThreadData* td
 				UnmakeMove(move, pos);
 
 				if (probcutScore >= probCutBeta) {
-					StoreHashEntry(pos->posKey, MoveToTT(move), ScoreToTT(probcutScore, ss->ply), ss->static_eval, HFLOWER, depth - 3, pvNode, ttPv);
+					if (ttScore == score_none || tte.depth < depth - 3)
+						StoreHashEntry(pos->posKey, MoveToTT(move), ScoreToTT(probcutScore, ss->ply), ss->static_eval, HFLOWER, depth - 3, pvNode, ttPv);
 					return probcutScore;
 				}
 					
