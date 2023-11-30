38f27604fcf4b8ef55330f81b14a79eb83b17f9e
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-30 08:33:42 +0800

    Progress. Bench 7902466

--- src/movepicker.h
+++ src/movepicker.h
@@ -1,5 +1,35 @@
+#include "search.h"
+#include "types.h"
+
 #pragma once
 
 struct S_MOVELIST;
 
+enum {
+    PICK_TT,
+    GEN_CAPTURES,
+    PICK_GOOD_CAPTURES,
+    PICK_KILLER_0,
+    PICK_KILLER_1,
+    PICK_COUNTER,
+    GEN_QUIET,
+    PICK_QUIET,
+    PICK_BAD_CAPTURES
+};
+
+struct Movepicker {
+    S_Board* pos;
+    Search_data* sd;
+    Search_stack* ss;
+    S_MOVELIST* goodCaptures;
+    S_MOVELIST* quiets;
+    S_MOVELIST* badCaptures;
+    int ttMove;
+    int threshold;
+    int idx;
+    int stage;
+};
+
 void PickMove(S_MOVELIST* moveList, const int moveNum);
+
+void InitMP(Movepicker *mp, S_Board* pos, Search_data* sd, Search_stack* ss, S_MOVELIST* move_list, int ttMove, int threshold);
