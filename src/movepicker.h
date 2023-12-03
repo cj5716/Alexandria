830ccb48f3b8935fa6ebcadbe71e55c072e0d919
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-12-03 10:13:59 +0800

    Bench 8349819

--- src/movepicker.h
+++ src/movepicker.h
@@ -26,11 +26,14 @@ struct Movepicker {
     S_MOVELIST quiets[1];
     S_MOVELIST badCaptures[1];
     int ttMove;
+    int killer0;
+    int killer1;
+    int counter;
     int threshold;
     int idx;
     int stage;
     bool capturesOnly;
 };
 
-void InitMP(Movepicker *mp, S_Board* pos, Search_data* sd, Search_stack* ss, int ttMove, int threshold, bool capturesOnly);
+void InitMP(Movepicker *mp, S_Board* pos, Search_data* sd, Search_stack* ss, int ttMove, int killer0, int killer1, int counter, int threshold, bool capturesOnly);
 int NextMove(Movepicker *mp, bool skipQuiets);
