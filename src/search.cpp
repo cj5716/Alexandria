1c6703deff230a62d5e86f283cda51a56175854c
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-12-17 08:49:25 +0800

    Bench 7548335

--- src/search.cpp
+++ src/search.cpp
@@ -676,8 +676,9 @@ moves_loop:
                 // Reduce more if we aren't improving
                 depthReduction += !improving;
 
-                // Reduce more if we are in an expected ALL node that hasn't been on the PV
-                depthReduction += !ttPv && !cutNode;
+                // Reduce more if we haven't been on the PV, and even more if we are on an expected ALL node
+                if (!ttPv)
+                    depthReduction += 1 + !cutNode;
 
                 // Fuck
                 depthReduction += 2 * cutNode;
