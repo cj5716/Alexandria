29f46c49f3d4d0b92e881d897faae75646454661
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-04-12 21:26:37 +0800

    Bench 8392603

--- src/nnue.h
+++ src/nnue.h
@@ -49,8 +49,8 @@ public:
     void update(NNUE::accumulator& board_accumulator, std::vector<NNUEIndices>& NNUEAdd, std::vector<NNUEIndices>& NNUESub);
     void addSub(NNUE::accumulator& board_accumulator, NNUEIndices add, NNUEIndices sub);
     void addSubSub(NNUE::accumulator& board_accumulator, NNUEIndices add, NNUEIndices sub1, NNUEIndices sub2);
-    [[nodiscard]] float ActivateFTAndAffineL1(const int16_t *inputs, const int16_t *weights, const float bias);
-    [[nodiscard]] float ActivateL1AndAffineL2(const float *inputs, const float *weights, const float bias);
+    void ActivateFTAndAffineL1(const int16_t *inputs, const int16_t *weights, const float *biases, float *output);
+    void ActivateL1AndAffineL2(const float *inputs, const float *weights, const float bias, float &output);
     [[nodiscard]] int32_t output(const NNUE::accumulator& board_accumulator, const bool whiteToMove, const int outputBucket);
     [[nodiscard]] NNUEIndices GetIndex(const int piece, const int square);
     #if defined(USE_AVX2)
