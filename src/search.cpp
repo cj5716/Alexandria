9dd5b72a7c5b3ede16f21fbc9fe455f02648c523
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-05-03 11:22:57 +0800

    Bench 6314275

--- src/search.cpp
+++ src/search.cpp
@@ -404,12 +404,6 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
 
     const bool ttPv = pvNode || (ttHit && FormerPV(tte.ageBoundPV));
 
-    // IIR by Ed Schroder (That i find out about in Berserk source code)
-    // http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
-    // https://github.com/jhonnold/berserk/blob/dd1678c278412898561d40a31a7bd08d49565636/src/search.c#L379
-    if (depth >= 4 && ttBound == HFNONE)
-        depth--;
-
     // clean killers and excluded move for the next ply
     (ss + 1)->excludedMove = NOMOVE;
     (ss + 1)->searchKiller = NOMOVE;
@@ -509,6 +503,14 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                 return razorScore;
         }
     }
+
+    // IIR by Ed Schroder (That i find out about in Berserk source code)
+    // http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
+    // https://github.com/jhonnold/berserk/blob/dd1678c278412898561d40a31a7bd08d49565636/src/search.c#L379
+    if ((pvNode || cutNode)
+        && ttMove == NOMOVE
+        && depth >= 4)
+        depth--;
 
 moves_loop:
 
