421ecb964b686dae4bf271c22986bb83c94c1303
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-07-12 07:23:03 +0800

    Bench 5624507

--- src/search.cpp
+++ src/search.cpp
@@ -651,7 +651,7 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
         info->nodes++;
         const uint64_t nodesBeforeSearch = info->nodes;
         // Conditions to consider LMR. Calculate how much we should reduce the search depth.
-        if (totalMoves > 1 + pvNode && depth >= 3 && (isQuiet || !ttPv)) {
+        if (totalMoves > 1 + pvNode + 4 * !isQuiet && depth >= 3) {
 
             // Get base reduction value
             int depthReduction = reductions[isQuiet][std::min(depth, 63)][std::min(totalMoves, 63)];
@@ -684,6 +684,18 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                 // Fuck
                 if (cutNode)
                     depthReduction += 2;
+
+                // Decrease the reduction for moves that give check
+                if (pos->checkers)
+                    depthReduction -= 1;
+
+                // Reduce less if we have been on the PV
+                if (ttPv)
+                    depthReduction -= 1 + cutNode;
+
+                // Reduce less for good captures
+                if (mp.stage == PICK_GOOD_NOISY)
+                    depthReduction -= 2;
 
                 // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
                 depthReduction -= moveHistory / 6144;
