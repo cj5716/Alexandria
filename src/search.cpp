4fde1d3c77a67406287dafc52ba9b2cdfc159e0f
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-07-18 11:51:18 +0800

    Bench 5633367

--- src/search.cpp
+++ src/search.cpp
@@ -689,13 +689,13 @@ int Negamax(int alpha, int beta, int depth, const bool cutNode, ThreadData* td, 
                 if (pos->checkers)
                     depthReduction -= 1;
 
-                // Reduce less for good captures
-                if (mp.stage == PICK_GOOD_NOISY)
-                    depthReduction -= 1;
-
                 // Reduce less if we have been on the PV
                 if (ttPv)
                     depthReduction -= 1 + cutNode;
+
+                // Reduce less for good captures
+                if (mp.stage == PICK_GOOD_NOISY)
+                    depthReduction -= 2;
 
                 // Decrease the reduction for moves that have a good history score and increase it for moves with a bad score
                 depthReduction -= moveHistory / 6144;
