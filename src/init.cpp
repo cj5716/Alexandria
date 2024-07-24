5e77233445cbd737ec1a206dda5fc55dc755537c
cj5716 <125858804+cj5716@users.noreply.github.com>
2024-07-24 20:14:55 +0800

    Bench 31424875

--- src/init.cpp
+++ src/init.cpp
@@ -138,7 +138,12 @@ void initializeLookupTables() {
 }
 
 // PreCalculate the logarithms used in the reduction calculation
-void InitReductions() {}
+void InitReductions() {
+    for (int depth = 0; depth < 64; ++depth) {
+        seeMargins[0][depth] = -30.0 * std::pow(depth, 2.0); // Noisy SEE margin
+        seeMargins[1][depth] = -80.0 * std::pow(depth, 1.0); // Quiet SEE margin
+    }
+}
 
 void initCuckoo(){
     // keep a total tally of the table entries to sanity check the init
